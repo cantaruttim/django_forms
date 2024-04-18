308985fa9a779f9df080252afb5b39be8d1f995c
Matheus Cantarutti <cantaruttim@outlook.com>
2024-04-17 23:32:54 -0300

    a simple form with django

---
+++ core/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContatoForm
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        print(f'POST: {request.POST}')
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+            print('Mensagem Enviada')
+            print(f'Nome: {nome}')
+            print(f'Email: {email}')
+            print(f'Assunto: {assunto}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar mensagem')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+
+def produto(request):
+    return render(request, 'produto.html')
