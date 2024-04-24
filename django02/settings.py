554230fdc43e72b83f6ca586f87327540272d0ca
Matheus Cantarutti <cantaruttim@outlook.com>
2024-04-23 21:00:19 -0300

    teste

--- django02/settings.py
+++ django02/settings.py
@@ -83,17 +83,23 @@ WSGI_APPLICATION = 'django02.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django02',
+#         'USER': 'root',
+#         'PASSWORD': '12102021Mc',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django02',
-        'USER': 'root',
-        'PASSWORD': '12102021Mc',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
